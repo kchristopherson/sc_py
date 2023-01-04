bcc31b87c617ac9a9bd43cbe9ad1f744b099ef00
Kyle Christopherson <kchristopherson@silvercreekcapital.com>
2023-01-04 12:51:04 -0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sc_py',
+      version='0.1',
+      description='Silver Creek internal python code',
+      url='https://github.com/kchristopherson/sc_py',
+      author='Kyle Christopherson',
+      author_email='kchristopherson@silvercreekcapital.com',
+      packages=['sc_py'],
+     # zip_safe=False
+     )
