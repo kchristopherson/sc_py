0ac28688bee4bc697f1b833a3ae6580e8ef9974f
Kyle Christopherson <>
2023-06-27 16:29:23 -0700

    fix get_liquidity fxn

--- sc_py/sc_fxns.py
+++ sc_py/sc_fxns.py
@@ -1048,6 +1048,8 @@ def get_liquidity(source, liquidity_df, better_sources):
     connection_string = 'Driver={SQL Server};Server=scdb1.silvercreeksv.com;Database=scfundrisk;Trusted_Connection=yes;'
     connection_url = URL.create(
         "mssql+pyodbc", query={"odbc_connect": connection_string})
+    engine = create_engine(connection_url)
+
 
     if type(better_sources) is not list:
         raise ValueError("""'better_sources' must be of type list """)
